173e7da5cf42c7c4fe94e1473a2c9515715dca53
Sam Scott-Hake <sam_hake@live.co.uk>
2015-06-28 23:21:48 +0100

    Modularise the http service into it's own service.

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,27 @@
 //App module created.
 var app = angular.module('app',[]);
 
+//Create a service to grab the posts from the api.
+app.service('PostsSvc', function($http) {
+	this.fetch = function() {
+		return $http.get('/api/posts');
+	}
+
+	this.create = function(post) {
+		return $http.post('/api/posts', post);
+	}
+})
+
 //Create post controller and dependency inject scope.
-app.controller('PostsCtrl', function($scope, $http) {
-	$http.get('http://localhost:3000/api/posts')
-	.success(function(posts) {
+app.controller('PostsCtrl', function($scope, PostsSvc) {
+
+	PostsSvc.fetch().success(function(posts) {
 		$scope.posts = posts;
-		console.log(posts);
-	})
-	.error(function(err) {
-		console.log(err);
 	})
 
 	$scope.addPost = function() {
 		if($scope.postBody) {
-			$http.post('/api/posts', {
+			PostsSvc.create( {
 				username:'SAM',
 				body: $scope.postBody
 			}).success(function(post) {
